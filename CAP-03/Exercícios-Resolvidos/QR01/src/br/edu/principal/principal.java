e5d1bbf6e48f1263a507311d8cb3dc28a5b82785
Isaac Ramos Magalhães <160159585+IsaacMaf@users.noreply.github.com>
2024-03-25 19:12:58 -0300

    Add files via upload

---
+++ CAP-03/Exercícios-Resolvidos/QR01/src/br/edu/principal/principal.java
@@ -0,0 +1,26 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class principal {
+	/*
+	 * Faça um programa que receba quatro números inteiros,
+	 * calcule e mostre a soma desses números.
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double n1 ,n2 ,n3 , n4 = 0;
+		System.out.println("este programa calcula a soma das notas 1, 2, 3 e 4, digitar por favor a 1: ");
+		n1 = sc.nextDouble();
+		System.out.println("digitar a 2 nota");
+		n2 = sc.nextDouble();
+		System.out.println("digitar a 3 nota");
+		n3 = sc.nextDouble();
+		System.out.println("digitar a 4 nota");
+		n4 = sc.nextDouble();
+		double soma = n1 + n2 + n3 + n4;
+		System.out.println("a soma de suas notas foram:" + soma);
+	}
+
+}
