0c483d2b00336c1e9e3c0fab6dc2a7eba58a62ba
Isaac Ramos Magalhães <160159585+IsaacMaf@users.noreply.github.com>
2024-03-25 14:59:58 -0300

    Add files via upload

--- CAP-03/Exercícios-Resolvidos/principal.java
+++ CAP-03/Exercícios-Resolvidos/principal.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 
 public class principal {
 
+	/*
+	 * Sabe-se que o quilowatt de energia custa um quinto do salário mínimo. Faça um programa que receba
+       o valor do salário mínimo e a quantidade de quilowatts consumida por uma residência. Calcule e
+       mostre:
+
+             a) o valor de cada quilowatt;
+             b) o valor a ser pago por essa residência;
+             c) o valor a ser pago com desconto de 15%.
+	 */
 	public static void main(String[] args) {
-		
-		/*
-		 * Uma pessoa deseja pregar um quadro em uma parede. Faça um programa para calcular e mostrar a
-           que distância a escada deve estar da parede. A pessoa deve fornecer o tamanho da escada e a altura em
-           que deseja pregar o quadro.Lembre-se de que o tamanho da escada deve ser maior que a altura que se 
-           deseja alcançar.
-           
-           X – Altura em que deseja pregar o quadro
-           Y – Distância em que deverá ficar a escada
-           Z – Tamanho da escada
-		 */
-		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("ATENÇÃO!! escala em metros!");
-		double x,y= 0;
-		System.out.println("\n qual o tamanho da escada?");
-		y = sc.nextDouble();
-		System.out.println("\n qual a altura que você deseja colocar o quadro?");
-		x = sc.nextDouble();
 		
-		double z = Math.pow(y, 2) - Math.pow(x, 2);
-		z = Math.sqrt(z);
-		System.out.printf("\n A distância que a escada deve ficar é de %.2f",z);
-		sc.close();
+		double salmin,vlrsal, qntkw, reais, vlrdesc  = 0;
+		salmin = 1410;
+		System.out.println("Leia com atenção: O calculo a seguir é baseado em reais, se a sua moeda não for essa, talvez possa ocorrer erros. Obrigado por ler! \n");
+		System.out.println("\n qual é o seu salário?");
+		vlrsal = sc.nextDouble();
+		System.out.printf("qual a quantidade de QuiloWatts da sua residência?");
+		qntkw = sc.nextDouble();
+		double vlrkw =(salmin/5);
+		reais = qntkw * vlrkw;
+		double desc = reais * 0.15;
+		vlrdesc = reais - desc;
+		System.out.println("\n O valor do QuilloWatts é de: "+ vlrkw);
+		System.out.printf("\n Você teria que pagar %.2f", reais ,"mas olhe!");
+		System.out.printf("\n\n Que sortudo você é! Temos um desconto para você! o total a pagar deve ser de: %.2f", vlrdesc);
+        System.out.println("\n\n E claro, o valor do desconto foi de "+ desc +"reais!");
+        sc.close();
 	}
 
 }
